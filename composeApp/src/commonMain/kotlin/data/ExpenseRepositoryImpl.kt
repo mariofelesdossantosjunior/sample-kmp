ae798fcde7263ad470e772791300873b66a7887e
Mario Feles dos Santos Junior <mario_feles@live.com>
2024-12-01 22:12:05 -0300

    feat: update method api

--- composeApp/src/commonMain/kotlin/data/ExpenseRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/data/ExpenseRepositoryImpl.kt
@@ -8,6 +8,7 @@ import io.ktor.client.call.body
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
@@ -87,7 +88,7 @@ class ExpenseRepositoryImpl(
             )
         }
 
-        httpClient.post(urlString = "$BASE_URL/expenses/${expense.id}") {
+        httpClient.put(urlString = "$BASE_URL/expenses/${expense.id}") {
             contentType(ContentType.Application.Json)
             setBody(ExpenseDTO.mapFrom(expense))
         }
